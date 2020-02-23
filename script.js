7460b6453fb83b80620ec097ba3cbee300e35fcc
kkalininMI <kkalinin@umich.edu>
2020-02-22 23:45:08 -0800

    Modefied js file

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+'use strict';
 
-var money = +prompt("Ваш бюджет на месяц?", 0);
-var time = prompt("Введите дату в формате YYYY-MM-DD", null);
+let money = +prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-var item1 = prompt("Введите обязательную статью расходов в этом месяце", null);
-var cost1 = prompt("Введите обязательную статью расходов в этом месяце", null);
-response1 = {item1:cost1};
-var item2 = prompt("Введите обязательную статью расходов в этом месяце", null);
-var cost2 = prompt("Введите обязательную статью расходов в этом месяце", null);
-response2 = {item2:cost2};
+let appData = {
+    budget: money, 
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time, 
+    savings: false
+};
 
-var appData = {budget: money, 
-               timeData: time, 
-               expenses: {response1: response2},
-               optionalExpenses: null,
-               income: null,
-               savings: false
-            };
 
-alert ("Бюджет за один день " + money/30);
+for (let i=0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if( (typeof(a))==='string' && (typeof(a)!=null) && (typeof(b)!=null)
+        && a!= '' && b !='' && a.length<50){
+        console.log("done");
+        appData.expenses[a] = b;
+        } else {
+
+        }
+
+    
+}
+appData.moneyPerDay = appData.budget / 30
+alert ("Еженедельный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay<100){
+    console.log("Минимальный уровень достатка");
+} else if(appData.moneyPerDay >100 && appData.moneyPerDay<2000){
+    console.log("Средний уровень достатка");
+} else if(appData.moneyPerDay >2000){
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");   
+}
 
